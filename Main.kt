20919599f29a2e876a8315dbed71fdca61cc2de1
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-28 09:35:57 +0900

    new day

--- Main.kt
+++ Main.kt
@@ -1,2 +1,27 @@
 class Main {
 }
+// класс для заполнения бланка ответов
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty : String
+)
+
+// класс для ответов правда ли лож
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+
+//класс для ответов по номерам
+class NumberQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
+// Создадим класс с универсальным типом данных generic
+class Question<T> (
+
+)
